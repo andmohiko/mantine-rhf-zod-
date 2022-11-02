e1835a34827bc035faf31efbddb1a75a2e1e205b
andmohiko <andmohiko@gmail.com>
2022-11-02 20:41:34 +0900

    feat: register inputs to rhf

--- src/components/Forms/SignUpForm.tsx
+++ src/components/Forms/SignUpForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import {
   Button,
   Checkbox,
@@ -10,8 +12,25 @@ import {
 } from '@mantine/core'
 import { DatePicker } from '@mantine/dates'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 export const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm()
+  const [birthday, setBirthday] = useState<Date | null>(new Date())
+  const [gender, setGender] = useState<string | null>()
+
+  const onSubmit = (data: any) => {
+    console.log('submit', {
+      ...data,
+      birthday,
+      gender
+    })
+  }
+
   return (
     <Stack
       spacing="xl"
@@ -21,11 +40,16 @@ export const SignUpForm = () => {
     >
       <Title order={1}>ログイン</Title>
 
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Stack>
           <Stack spacing="md">
-            <TextInput label="ユーザー名" placeholder="" />
-            <DatePicker label="生年月日" />
+            <TextInput label="ユーザー名" {...register('username')} />
+            <DatePicker
+              label="生年月日"
+              placeholder="日付を選択してください"
+              value={birthday}
+              onChange={setBirthday}
+            />
             <Select
               label="性別"
               placeholder="ひとつ選んでください"
@@ -34,9 +58,15 @@ export const SignUpForm = () => {
                 { value: 'male', label: '男性' },
                 { value: 'unknown', label: 'その他' }
               ]}
+              value={gender}
+              onChange={setGender}
             />
-            <TextInput label="メールアドレス" placeholder="email@example.com" />
-            <PasswordInput label="パスワード" />
+            <TextInput
+              label="メールアドレス"
+              placeholder="email@example.com"
+              {...register('email')}
+            />
+            <PasswordInput label="パスワード" {...register('password')} />
             <Checkbox label="利用規約に同意しました" />
           </Stack>
 
@@ -44,7 +74,9 @@ export const SignUpForm = () => {
             <Link href="#">パスワードを忘れた場合はこちら</Link>
           </Text>
 
-          <Button type="submit">ログイン</Button>
+          <Button type="submit" loading={isSubmitting}>
+            ログイン
+          </Button>
         </Stack>
       </form>
     </Stack>
