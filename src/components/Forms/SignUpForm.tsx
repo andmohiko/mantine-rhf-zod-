fbba6b39d7e9de35bdd22e395ad88e8bf4141d37
andmohiko <andmohiko@gmail.com>
2023-05-14 18:58:02 +0900

    feat: use controller to register values

--- src/components/Forms/SignUpForm.tsx
+++ src/components/Forms/SignUpForm.tsx
@@ -9,19 +9,21 @@ import {
   Select,
   Text,
   TextInput,
-  Title
+  NativeSelect,
 } from '@mantine/core'
-import { DatePicker } from '@mantine/dates'
+import { DatePickerInput } from '@mantine/dates'
 import Link from 'next/link'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 import { z } from 'zod'
 
 const SignUpSchema = z.object({
-  username: z.string().min(1, { message: 'ユーザー名を入力してください' }),
+  birthday: z.date(),
   email: z.string().email(),
+  gender: z.string(),
   password: z
     .string()
-    .min(8, { message: 'パスワードは8文字以上にしてください' })
+    .min(8, { message: 'パスワードは8文字以上にしてください' }),
+  username: z.string().min(1, { message: 'ユーザー名を入力してください' }),
 })
 
 type SignUpInputType = z.infer<typeof SignUpSchema>
@@ -30,88 +32,90 @@ export const SignUpForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting }
+    control,
+    formState: { errors, isSubmitting },
   } = useForm<SignUpInputType>({ resolver: zodResolver(SignUpSchema) })
 
-  const [birthday, setBirthday] = useState<Date | null>()
   const [gender, setGender] = useState<string | null>()
   const [checked, setChecked] = useState<boolean>(false)
 
   const onSubmit: SubmitHandler<SignUpInputType> = (data) => {
     console.log('submit', {
       ...data,
-      birthday,
-      gender
+      gender,
     })
   }
 
   return (
-    <Stack
-      spacing="xl"
-      style={{
-        width: '480px'
-      }}
-    >
-      <Title order={1}>ユーザー登録</Title>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack>
-          <Stack spacing="md">
-            <TextInput
-              label="ユーザー名"
-              required
-              error={errors.username?.message}
-              {...register('username')}
-            />
-            <DatePicker
-              label="生年月日"
-              placeholder="日付を選択してください"
-              required
-              value={birthday}
-              onChange={setBirthday}
-            />
-            <Select
-              label="性別"
-              placeholder="ひとつ選んでください"
-              data={[
-                { value: 'female', label: '女性' },
-                { value: 'male', label: '男性' },
-                { value: 'unknown', label: 'その他' }
-              ]}
-              required
-              value={gender}
-              onChange={setGender}
-            />
-            <TextInput
-              label="メールアドレス"
-              placeholder="email@example.com"
-              required
-              error={errors.email?.message}
-              {...register('email')}
-            />
-            <PasswordInput
-              label="パスワード"
-              required
-              error={errors.password?.message}
-              {...register('password')}
-            />
-            <Checkbox
-              label="利用規約に同意しました"
-              required
-              checked={checked}
-              onChange={(event) => setChecked(event.currentTarget.checked)}
-            />
-          </Stack>
-
-          <Text size="xs">
-            <Link href="#">パスワードを忘れた場合はこちら</Link>
-          </Text>
-
-          <Button type="submit" disabled={!checked} loading={isSubmitting}>
-            登録する
-          </Button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <Stack spacing="md">
+          <TextInput
+            label="ユーザー名"
+            required
+            error={errors.username?.message}
+            {...register('username')}
+          />
+          <Controller
+            name="birthday"
+            control={control}
+            render={({ field }) => (
+              <DatePickerInput
+                label="生年月日"
+                placeholder="日付を選択してください"
+                required
+                value={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+          <Controller
+            name="gender"
+            control={control}
+            render={({ field }) => (
+              <NativeSelect
+                label="性別"
+                placeholder="ひとつ選んでください"
+                data={[
+                  { value: 'female', label: '女性' },
+                  { value: 'male', label: '男性' },
+                  { value: 'unknown', label: 'その他' },
+                ]}
+                required
+                value={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+          <TextInput
+            label="メールアドレス"
+            placeholder="email@example.com"
+            required
+            error={errors.email?.message}
+            {...register('email')}
+          />
+          <PasswordInput
+            label="パスワード"
+            required
+            error={errors.password?.message}
+            {...register('password')}
+          />
+          <Checkbox
+            label="利用規約に同意しました"
+            required
+            checked={checked}
+            onChange={(event) => setChecked(event.currentTarget.checked)}
+          />
         </Stack>
-      </form>
-    </Stack>
+
+        <Text size="xs">
+          <Link href="#">パスワードを忘れた場合はこちら</Link>
+        </Text>
+
+        <Button type="submit" disabled={!checked} loading={isSubmitting}>
+          登録する
+        </Button>
+      </Stack>
+    </form>
   )
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,10 +1,16 @@
-import { SignUpForm } from '@/components/Forms/SignUpForm'
-import { DefaultLayout } from '@/components/Layouts/DefaultLayout'
+import { Title } from '@mantine/core'
+
+import { FlexBox } from '~/components/Base/FlexBox'
+import { SignUpForm } from '~/components/Forms/SignUpForm'
+import { DefaultLayout } from '~/components/Layouts/DefaultLayout'
 
 const SignUpPage = () => {
   return (
     <DefaultLayout>
-      <SignUpForm />
+      <FlexBox width={500} gap={40} align="stretch">
+        <Title order={1}>ユーザー登録</Title>
+        <SignUpForm />
+      </FlexBox>
     </DefaultLayout>
   )
 }
