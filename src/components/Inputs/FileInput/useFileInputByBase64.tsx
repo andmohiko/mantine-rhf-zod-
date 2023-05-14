0e001b3e7432054d646a028a31e9800be4f5abef
andmohiko <andmohiko@gmail.com>
2023-05-14 21:31:35 +0900

    fix: accept multi select for base64

--- src/components/Inputs/FileInput/useFileInputByBase64.tsx
+++ src/components/Inputs/FileInput/useFileInputByBase64.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import { notifications } from '@mantine/notifications'
 import { BiErrorCircle } from 'react-icons/bi'
@@ -27,52 +27,59 @@ export const useFileInput = (
   const [loading, setLoading] = useState<boolean>(false)
   const disabled = files.length >= max
 
-  const addFile = (file: FileObject) => {
-    setFiles([...files.slice(), file])
-  }
+  const removeFile = useCallback(
+    (index: number) => {
+      const newFiles = files.slice()
+      newFiles.splice(index, 1)
+      setFiles(newFiles)
+    },
+    [files, setFiles],
+  )
 
-  const removeFile = (index: number) => {
-    const newFiles = files.slice()
-    newFiles.splice(index, 1)
-    setFiles(newFiles)
-  }
-
-  const onChange = (files: Array<File>) => {
-    if (!files || files.length === 0) {
-      return ''
-    }
-
-    const file = files[0]
-    try {
-      setLoading(true)
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
-      reader.onloadend = () => {
-        const { result } = reader as { result: string }
-        if (
-          result.startsWith('data:image/') ||
-          result.startsWith('data:video/mp4')
-        ) {
-          addFile({
-            file_base64: result,
-            file_name: file.name,
-            object_url: URL.createObjectURL(file),
-          })
-        }
+  const onChange = useCallback(
+    async (inputFiles: Array<File>) => {
+      if (!inputFiles || inputFiles.length === 0) {
+        return ''
       }
-    } catch {
-      notifications.show({
-        title: 'ファイルのアップロードに失敗しました',
-        message: '',
-        icon: <BiErrorCircle />,
-        withCloseButton: true,
-        autoClose: 8000,
-        color: 'red',
-      })
-    } finally {
-      setLoading(false)
-    }
-  }
+
+      try {
+        setLoading(true)
+        const newFiles: Array<FileObject> = []
+        for (let i = 0; i < inputFiles.length; i++) {
+          const file = inputFiles[i]
+          const reader = new FileReader()
+          reader.readAsDataURL(file)
+          await new Promise<void>(
+            (resolve) =>
+              (reader.onloadend = () => {
+                const { result } = reader as { result: string }
+                if (result.startsWith('data:image/')) {
+                  newFiles.push({
+                    file_base64: result,
+                    file_name: file.name,
+                    object_url: URL.createObjectURL(file),
+                  })
+                }
+                resolve()
+              }),
+          )
+        }
+        setFiles([...files, ...newFiles])
+      } catch {
+        notifications.show({
+          title: 'ファイルのアップロードに失敗しました',
+          message: '',
+          icon: <BiErrorCircle />,
+          withCloseButton: true,
+          autoClose: 8000,
+          color: 'red',
+        })
+      } finally {
+        setLoading(false)
+      }
+    },
+    [files, setFiles],
+  )
 
   return [
     files,
